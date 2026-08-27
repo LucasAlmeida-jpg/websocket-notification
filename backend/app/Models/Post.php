a47dd6e94fd2f25185be77e3de9b2d45caf87ce7
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2026-08-27 08:21:33 -0300

    feat: add HasFactory trait to Post model for factory support

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    use HasFactory;
     protected $fillable = ['user_id', 'parent_id', 'body', 'likes_count', 'replies_count', 'reposts_count'];
 
     protected $casts = ['likes_count' => 'integer', 'replies_count' => 'integer', 'reposts_count' => 'integer'];
